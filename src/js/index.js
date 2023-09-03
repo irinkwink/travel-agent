d3f4955c98873787812f0986de299f397556f02a
irinkwink <irina-ns@yandex.ru>
2023-09-03 14:18:57 +0300

    feat: add Partners, Popular, Testimonials section

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,18 @@ import datePicker from "./modules/datePicker";
 autoCompleteLocation();
 datePicker();
 
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 32,
+  navigation: {
+    nextEl: '#sliderNext',
+    prevEl: '#sliderPrev',
+  },
+});
+
 
 
 // import mobileNav from './modules/mobile-nav.js';
